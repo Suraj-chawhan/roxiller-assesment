0cb7cbeebfae60ee831451ccad4b6385a1821d9f
Suraj Chawhan <112802275+Suraj-chawhan@users.noreply.github.com>
2025-09-08 20:56:01 +0530

    Create .eslintrc.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["react"],
+  settings: { react: { version: "detect" } },
+};
